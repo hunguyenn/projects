7ce08dc8651ca1de0d77a57c884a6edc0abd3607
Hung Nguyen <hg.ngyn@gmail.com>
2015-12-18 21:52:31 -0800

    create env file

--- wunderlist_cli/index.js
+++ wunderlist_cli/index.js
@@ -1,3 +1,32 @@
 #! /usr/bin/env node
-// process.argv -> command line arguments
-console.log(process.argv);
+
+var request = require('request');
+var auth = require('./auth.js');
+var env = require('node-env-file');
+var fs = require('fs');
+
+// put in try catch -> need to authorize if gets in catch
+env(__dirname + '/.env');
+
+switch (process.argv[2]) {
+	case "auth":
+		auth(process.argv[3], process.argv[4]);
+		break;
+	default:
+		
+		console.log(process.env.CLIENT_ID);
+}
+
+
+
+// auth -> set environment variables
+
+
+
+
+
+// supported commands
+// authorize
+// avatar -> converts your pic into text characters
+// handle command line arguments in one method
+// each command gets their own file
